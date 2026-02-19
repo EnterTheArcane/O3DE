87d78a047f6acc3c8199df8142e52ebc18742397
EnterTheArcane <96613937+EnterTheArcane@users.noreply.github.com>
2026-02-18 19:18:37 -0700

    Replace void_t

--- Code/Framework/AzFramework/AzFramework/Components/ComponentAdapterHelpers.h
+++ Code/Framework/AzFramework/AzFramework/Components/ComponentAdapterHelpers.h
@@ -15,54 +15,29 @@ namespace AzFramework
 {
     namespace Components
     {
-        //////////////////////////////////////////////////////////////////////////
-        // Helper functions to make certain functions optional in the class wrapped by
-        // EditorComponentAdapter and ComponentAdapter.
-
         // Make Init() Optional
 
-        template<typename T, typename = void>
-        struct ComponentInitHelper
-        {
-            static void Init([[maybe_unused]] T& controller)
-            {
-            }
-        };
-
         template<typename T>
-        struct ComponentInitHelper<T, AZStd::void_t<decltype(AZStd::declval<T>().Init())>>
+        void TryInit(T& controller)
         {
-            static void Init(T& controller)
+            if constexpr (requires(T& type) { type.Init(); })
             {
                 controller.Init();
             }
-        };
-
-        template<typename T, typename = void>
-        struct ComponentActivateHelper
-        {
-            static void Activate([[maybe_unused]] T& controller, [[maybe_unused]] const AZ::EntityComponentIdPair& entityComponentIdPair)
-            {
-            }
-        };
+        }
 
         template<typename T>
-        struct ComponentActivateHelper<T, AZStd::void_t<decltype(AZStd::declval<T>().Activate(AZ::EntityId()))>>
+        void TryActivate(T& controller, const AZ::EntityComponentIdPair& entityComponentIdPair)
         {
-            static void Activate(T& controller, const AZ::EntityComponentIdPair& entityComponentIdPair)
-            {
-                controller.Activate(entityComponentIdPair.GetEntityId());
-            }
-        };
-
-        template<typename T>
-        struct ComponentActivateHelper<T, AZStd::void_t<decltype(AZStd::declval<T>().Activate(AZ::EntityComponentIdPair()))>>
-        {
-            static void Activate(T& controller, const AZ::EntityComponentIdPair& entityComponentIdPair)
+            if constexpr (requires(T& type, const AZ::EntityComponentIdPair& pair) { type.Activate(pair); })
             {
                 controller.Activate(entityComponentIdPair);
             }
-        };
+            else if constexpr (requires(T& type, AZ::EntityId id) { type.Activate(id); })
+            {
+                controller.Activate(entityComponentIdPair.GetEntityId());
+            }
+        }
 
         // Make GetProvidedServices, GetDependentServicesHelper, GetRequiredServices and GetIncompatibleServices optional.
 
